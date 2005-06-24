532d801265fb4701fd060c8c93614e3b658d9ddb
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2005-06-24 14:34:23 +0000

    Initial revision.

---
+++ vgBase/trunk/include/vgBase/Type.hpp
@@ -0,0 +1,38 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifdef _MSC_VER
+
+	// The Visual C++ debugger doesn't know string larger than 255 char. STL often creates large
+	// symbol. The following line cancels warning created by Visual.
+	#pragma warning (disable : 4786)
+
+	// need a dll interface (Un classe de base ou une structure doit être déclarée avec 
+	// le mot clé __declspec(dllexport) pour qu'une fonction d'une classe dérivée soit exportée)
+	#pragma warning (disable:4251)
+	
+	// The following line prevents from this file to be included several times by the compiler with
+	// Visual C++. This makes compiling quicker, but must not be used when your file content may
+	// change depending on some macro definitions. In the general case, leave these lines as they are.
+	#pragma once
+
+#endif
+
+#include <cassert>
+#include <typeinfo>
+
+// FIXME: TODO test if sizeof( followin types) must == 1 2 4...
+#ifdef __STDC__
+	typedef signed char		int8;
+#else
+	typedef char				int8;
+#endif
+typedef unsigned char		uint8;
+
+typedef short					int16;
+typedef unsigned short		uint16;
+
+typedef int						int32;
+typedef unsigned int			uint32;
---
+++ vgBase/trunk/include/vgBase/vgBase.hpp
@@ -0,0 +1,34 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifndef _VGBASE_VGBASE_H
+#define _VGBASE_VGBASE_H
+
+/** 
+ * @namespace vgBase
+ * 
+ * @brief Virtuals Graphical Base
+ *
+ * \b vgBase is open source (LGPL).
+ */
+
+
+#include <vgBase/Type.hpp>
+
+//#ifdef _WIN32
+//
+//   #ifdef VGBASE_EXPORTS
+//   #define VGBASE_API	__declspec(dllexport)
+//   #else
+//   #define VGBASE_API	__declspec(dllimport)
+//   #endif
+//
+//#else
+//
+//    #define VGBASE_API
+//
+//#endif
+
+#endif // #ifndef _VGBASE_VGBASE_H
