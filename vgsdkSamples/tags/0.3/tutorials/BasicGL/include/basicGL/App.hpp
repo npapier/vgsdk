0e18d1ef5e86304a1eb7e7c957a3807d7079d279
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2006-01-20 11:50:55 +0000

    Version 0.3

---
+++ vgsdkSamples/tags/0.3/tutorials/BasicGL/include/basicGL/App.hpp
@@ -0,0 +1,35 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifndef _BASICGL_APP_H
+#define _BASICGL_APP_H
+
+#include <wx/defs.h>
+#include <wx/glcanvas.h>
+
+
+
+namespace basicGL
+{
+
+
+
+/**
+ * The application class.
+ */
+class App : public wxGLApp
+{
+	DECLARE_EVENT_TABLE();
+
+public:
+
+	bool OnInit();
+};
+
+
+
+} // namespace basicGL
+
+#endif // #ifndef _BASICGL_APP_H
