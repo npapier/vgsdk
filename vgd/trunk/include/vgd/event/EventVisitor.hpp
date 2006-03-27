9c8d2a8257856c06fd5a8fcab4f7cc75736a0f43
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2006-03-27 08:19:16 +0000

    ~ Adds virtual destructors and removes some dependencies.

--- vgd/trunk/include/vgd/event/EventVisitor.hpp
+++ vgd/trunk/include/vgd/event/EventVisitor.hpp
@@ -1,10 +1,10 @@
-// VGSDK - Copyright (C) 2004, IRCAD.
+// VGSDK - Copyright (C) 2004-2006, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
 
-#ifndef _VGD_EVENT_EVENTVISITOR_H
-#define _VGD_EVENT_EVENTVISITOR_H
+#ifndef _VGD_EVENT_EVENTVISITOR_HPP
+#define _VGD_EVENT_EVENTVISITOR_HPP
 
 #include "vgd/vgd.hpp"
 
@@ -28,11 +28,16 @@ struct MouseWheelEvent;
  */
 struct VGD_API EventVisitor
 {
+	/**
+	 * @brief Virtual destructor
+	 */
+	virtual ~EventVisitor();
+
 	virtual void apply( const vgd::event::KeyboardButtonEvent	*pKeyboardButtonEvent	)=0;
-	virtual void apply( const vgd::event::Location2Event			*pLocation2Event 			)=0;
+	virtual void apply( const vgd::event::Location2Event		*pLocation2Event 		)=0;
 	virtual void apply( const vgd::event::MouseButtonEvent		*pMouseButtonEvent		)=0;
-	virtual void apply( const vgd::event::Motion3Event				*pMotion3Event				)=0;
-	virtual void apply( const vgd::event::MouseWheelEvent			*pMouseWheelEvent			)=0;
+	virtual void apply( const vgd::event::Motion3Event			*pMotion3Event			)=0;
+	virtual void apply( const vgd::event::MouseWheelEvent		*pMouseWheelEvent		)=0;
 };
 
 
@@ -41,4 +46,4 @@ struct VGD_API EventVisitor
 
 } // namespace vgd
 
-#endif //#ifndef _VGD_EVENT_EVENTVISITOR_H
+#endif //#ifndef _VGD_EVENT_EVENTVISITOR_HPP
--- vgd/trunk/include/vgd/event/Listener.hpp
+++ vgd/trunk/include/vgd/event/Listener.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004, IRCAD.
+// VGSDK - Copyright (C) 2004-2006, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -7,18 +7,26 @@
 #ifndef _VGD_EVENT_LISTENER_HPP
 #define _VGD_EVENT_LISTENER_HPP
 
-
 #include "vgd/vgd.hpp"
-#include "vgd/Shp.hpp"
-#include "vgd/event/Event.hpp"
+
+namespace vgd
+{
+	template<class T> struct Shp;
+
+	namespace event
+	{
+		struct Event;
+	}
+}
+
 
 
 namespace vgd
 {
-	
+
 namespace event
 {
-	
+
 /**
  * @brief	Defines the abstract interface for objects that can receive events.
  * 
@@ -30,12 +38,19 @@ namespace event
 struct VGD_API Listener
 {
 	/**
+	 * @brief Virtual destructor
+	 */
+	virtual ~Listener();
+
+	/**
 	 * @brief	Notifies the listener about the raise of an event.
 	 * 
 	 * @param	event	a shared pointer to the raised event
 	 */
 	virtual void onEvent( vgd::Shp<vgd::event::Event> event ) = 0;
 };
+
+
 	
 } // namespace event
 	
