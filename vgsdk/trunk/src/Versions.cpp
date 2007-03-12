9632087d24dea2a3f6a4b0ce3122fa9ad5cb0851
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2007-03-12 14:18:35 +0000

    - Removes Makefile. ~ Updates version to 0.4beta.

--- vgsdk/trunk/src/Versions.cpp
+++ vgsdk/trunk/src/Versions.cpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004-2006 Nicolas Papier.
+// VGSDK - Copyright (C) 2004-2006, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -14,7 +14,7 @@ namespace vgsdk
 
 std::string getVersion()
 {
-	return ( std::string("0.4") );
+	return ( std::string("0.4beta") );
 }
 
 
