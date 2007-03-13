65c0b047d23666b7a4fdfc5359e864db8e8a3cc8
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2007-03-13 14:51:27 +0000

    Copied vgsdkViewer project remotely

---
+++ vgsdk/trunk/examples/vgsdkViewer/src/App.cpp
@@ -0,0 +1,49 @@
+// VGSDK - Copyright (C) 2004, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include "vgsdkViewer/App.hpp"
+
+#include "vgsdkViewer/myCanvas.hpp"
+#include <wx/Frame.h>
+
+
+
+IMPLEMENT_APP( vgsdkViewer::App )
+
+namespace vgsdkViewer
+{
+
+
+
+BEGIN_EVENT_TABLE( App, wxApp )
+END_EVENT_TABLE()
+
+
+
+bool App::OnInit()
+{
+	wxFrame*	pFrame = new wxFrame( NULL, -1, _T("vgsdkViewer") );
+	pFrame->CreateStatusBar();		
+	SetTopWindow( pFrame );
+		
+	// Creation of the child GL canvas.
+	vgsdkViewer::myCanvas*	pCanvas					= new vgsdkViewer::myCanvas( pFrame );
+
+	wxLayoutConstraints*		pLayoutConstraints	= new wxLayoutConstraints();
+	pLayoutConstraints->left	.SameAs( pFrame, wxLeft	);
+	pLayoutConstraints->right	.SameAs( pFrame, wxRight	);
+	pLayoutConstraints->top		.SameAs( pFrame, wxTop	);
+	pLayoutConstraints->bottom	.SameAs( pFrame, wxBottom );
+	pCanvas->SetConstraints( pLayoutConstraints );
+	
+	//
+	pFrame->Show( true );
+
+	return true;
+}
+
+
+
+} // namespace vgsdkViewer
