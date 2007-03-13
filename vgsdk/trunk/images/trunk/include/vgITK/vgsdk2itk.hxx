c602750ead7dde0ca66969ba7677dda187b953af
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2007-03-13 14:44:59 +0000

    Copied trunk of vgITK remotely

---
+++ vgsdk/trunk/images/trunk/include/vgITK/vgsdk2itk.hxx
@@ -0,0 +1,53 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include <itkImportImageContainer.h>
+#include <vgd/basic/IImage.hpp>
+
+
+
+namespace vgITK
+{
+
+/**
+ * @brief Converts an vgsdk image into an itk image.
+ * 
+ * @remarks The image data is imported (no copy occurs). And the memory of the imaga data would not be freeing by the 
+ * ITK image.
+ * 
+ * @pre iimage != 0
+ */
+template< typename ItkImageType >
+typename ItkImageType::Pointer convertIImage2ItkImage( vgd::Shp< vgd::basic::IImage > iimage )
+{
+	assert( iimage != 0 );
+
+	typename ItkImageType::Pointer dstImage = ItkImageType::New();
+	
+	// Create the region for the ITK image
+	typename ItkImageType::SizeType size;
+	size[0] = iimage->width();
+	size[1] = iimage->height();
+	size[2] = iimage->depth();
+	typename ItkImageType::RegionType const region(size);
+	dstImage->SetRegions(region);
+	
+	// Get a pointer to the voxels in the IImage
+	typename ItkImageType::PixelType* voxels = reinterpret_cast<typename ItkImageType::PixelType*>(iimage->editPixels());
+	iimage->editPixelsDone();
+
+	// Assign the pointer to the ITK image container
+	typename itk::ImportImageContainer<uint32, typename ItkImageType::PixelType>::Pointer container;
+	container = itk::ImportImageContainer<uint32, typename ItkImageType::PixelType>::New();
+
+	container->SetImportPointer(voxels, iimage->computeNumberOfPixels(), false);
+
+	dstImage->SetPixelContainer(container);
+	
+	return dstImage;
+}
+
+
+} // namespace vgITK
---
+++ vgsdk/trunk/images/trunk/src/Failed.cpp
@@ -0,0 +1,27 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include "vgITK/Failed.hpp"
+
+
+
+namespace vgITK
+{
+
+
+
+Failed::Failed(const std::string &message) throw()
+: m_message(message)
+{}
+
+
+const char* Failed::what() const throw()
+{
+	return m_message.c_str();
+}
+
+
+
+} // namespace vgITK
