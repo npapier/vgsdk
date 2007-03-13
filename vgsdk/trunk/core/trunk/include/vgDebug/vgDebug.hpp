a287c18a939c658a3aec9f89128aedac22ffd208
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2007-03-13 11:24:36 +0000

    Copied trunk of vgDebug remotely

---
+++ vgsdk/trunk/core/trunk/include/vgDebug/vgDebug.hpp
@@ -0,0 +1,35 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifndef _VGDEBUG_H
+#define _VGDEBUG_H
+
+#include <vgBase/Type.hpp>
+
+#ifdef _WIN32
+
+   #ifdef VGDEBUG_EXPORTS
+   #define VGDEBUG_API	__declspec(dllexport)
+   #else
+   #define VGDEBUG_API	__declspec(dllimport)
+   #endif
+
+#else
+
+    #define VGDEBUG_API
+
+#endif
+
+
+
+/** 
+ * @namespace vgDebug
+ * 
+ * @brief Virtuals Graphical Debug
+ * 
+ * \b vge is open source (LGPL).
+ */
+
+#endif //#ifndef _VGDEBUG_H
---
+++ vgsdk/trunk/core/trunk/src/Logging.cpp
@@ -0,0 +1,207 @@
+// VGSDK - Copyright (C) 2004, IRCAD.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include <wx/defs.h>
+#include <wx/log.h>
+
+#include "vgDebug/Logging.hpp"
+
+
+
+namespace vgDebug
+{
+
+
+
+Logging::Logging() :
+	m_pLogger( new wxLogGui ) //wxLogChain(...) new wxLogGui() new wxLogStderr()
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);	
+	
+	logDebug("Create a new logger.");
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+Logging::~Logging()
+{
+	logDebug("Destroy a logger.");
+	
+	// Destroy logger.
+	if ( m_pLogger != 0 )
+	{
+		wxLog *pLogBak = wxLog::SetActiveTarget(NULL);
+		
+		delete m_pLogger;
+
+		if ( pLogBak != m_pLogger )
+		{
+			wxLog::SetActiveTarget( pLogBak );
+		}
+	}
+}
+
+
+
+void Logging::logFatalError( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogFatalError( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logError( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogError( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logWarning( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogWarning( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logMessage( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogMessage( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logVerbose( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogVerbose( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+	
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logStatus( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogStatus( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logSysError( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogSysError( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+
+void Logging::logDebug( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	wxVLogDebug( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::logTrace( const char *szFormat, ... ) const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+	
+	va_list marker;
+	va_start( marker, szFormat );
+
+	// wxVLogTrace( wxConvertMB2WX(szFormat), marker ); FIXME modified interface in wx2.5 and superior.
+	wxVLogDebug( wxConvertMB2WX(szFormat), marker );
+
+	va_end( marker );
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+void Logging::flush() const
+{
+	wxLog *pLogBak = wxLog::SetActiveTarget(m_pLogger);
+
+	m_pLogger->FlushActive();
+
+	wxLog::SetActiveTarget(pLogBak);
+}
+
+
+
+} // namespace vgDebug
