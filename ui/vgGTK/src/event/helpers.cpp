0cf72b867034e51c0fad14cdc4909701d48c995b
gbrocker <gbrocker@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-03-14 15:29:36 +0000

    ~ Changes output messages of the default logger. ~ Updates the GTK-based keyboard to support more keys.

--- ui/vgGTK/src/event/helpers.cpp
+++ ui/vgGTK/src/event/helpers.cpp
@@ -8,6 +8,7 @@
 
 #include <gdk/gdkkeysyms.h>
 #include <vgd/event/KeyboardButtonEvent.hpp>
+#include <vgDebug/Global.hpp>
 
 
 
@@ -95,7 +96,7 @@ const ::vgd::event::ButtonEvent::State getButtonState( const GdkEventKey * event
 const int getKeyboardButton( const GdkEventKey * event )
 {
 //@todo	Implementation to be done !
-	
+
 	using vgd::event::KeyboardButtonEvent;
 
 	switch( event->keyval )
@@ -213,7 +214,90 @@ const int getKeyboardButton( const GdkEventKey * event )
 //	KeyboardButtonEvent::KEY_NUMPAD_DECIMAL
 //	KeyboardButtonEvent::KEY_NUMPAD_DIVIDE
 
-	default:	return 0;
+	case GDK_0:	return '0';
+	case GDK_1:	return '1';
+	case GDK_2:	return '2';
+	case GDK_3:	return '3';
+	case GDK_4:	return '4';
+	case GDK_5:	return '5';
+	case GDK_6:	return '6';
+	case GDK_7:	return '7';
+	case GDK_8:	return '8';
+	case GDK_9:	return '9';
+
+	case GDK_colon:		return ',';
+	case GDK_semicolon:	return ';';
+	case GDK_less:		return '<';
+	case GDK_equal:		return '=';
+	case GDK_greater:	return '>';
+	case GDK_question:	return '?';
+	case GDK_at:		return '@';
+
+	case GDK_A:	return 'A';
+	case GDK_B:	return 'B';
+	case GDK_C:	return 'C';
+	case GDK_D:	return 'D';
+	case GDK_E:	return 'E';
+	case GDK_F:	return 'F';
+	case GDK_G:	return 'G';
+	case GDK_H:	return 'H';
+	case GDK_I:	return 'I';
+	case GDK_J:	return 'J';
+	case GDK_K:	return 'K';
+	case GDK_L:	return 'L';
+	case GDK_M:	return 'M';
+	case GDK_N:	return 'N';
+	case GDK_O:	return 'O';
+	case GDK_P:	return 'P';
+	case GDK_Q:	return 'Q';
+	case GDK_R:	return 'R';
+	case GDK_S:	return 'S';
+	case GDK_T:	return 'T';
+	case GDK_U:	return 'U';
+	case GDK_V:	return 'V';
+	case GDK_W:	return 'W';
+	case GDK_X:	return 'X';
+	case GDK_Y:	return 'Y';
+	case GDK_Z:	return 'Z';
+
+	case GDK_bracketleft:	return '[';
+	case GDK_backslash:		return '\\';
+	case GDK_bracketright:	return ']';
+	case GDK_asciicircum:	return '^';
+	case GDK_underscore:	return '_';
+	case GDK_grave:			return '`';
+//	case GDK_quoteleft:		return '\'';
+
+	case GDK_a: return 'a';
+	case GDK_b: return 'b';
+	case GDK_c: return 'c';
+	case GDK_d: return 'd';
+	case GDK_e: return 'e';
+	case GDK_f: return 'f';
+	case GDK_g: return 'g';
+	case GDK_h: return 'h';
+	case GDK_i: return 'i';
+	case GDK_j: return 'j';
+	case GDK_k: return 'k';
+	case GDK_l: return 'l';
+	case GDK_m: return 'm';
+	case GDK_n: return 'n';
+	case GDK_o: return 'o';
+	case GDK_p: return 'p';
+	case GDK_q: return 'q';
+	case GDK_r: return 'r';
+	case GDK_s: return 's';
+	case GDK_t: return 't';
+	case GDK_u: return 'u';
+	case GDK_v: return 'v';
+	case GDK_w: return 'w';
+	case GDK_x: return 'x';
+	case GDK_y: return 'y';
+	case GDK_z: return 'z';
+
+	default:
+		vgDebug::get().logDebug( "Unsupported keyboard key %c (%x).", event->keyval, event->keyval );
+		return 0;
 	}
 }
 
