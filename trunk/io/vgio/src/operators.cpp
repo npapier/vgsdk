385f7eceff8e2302ccb81fbb12204945b39d3ff4
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-05-27 15:10:05 +0000

    ~ Improves configuration of ostream by setting the precision for floating-point values to 3 and specifies that variables of type bool must be output as true or false. + Adds operator<<() for all field containers, vgd::Shp< vgd::node::Group>, vgd::Shp< vgd::node::Node>, vgm::Box3f, vgm::XfBox3f, vgm::Rectangle, vgm::Rotation, int8 and uint8. Takes into account special values (like empty or invalid boxes, null vector, identity matrix and so on).

--- trunk/io/vgio/src/operators.cpp
+++ trunk/io/vgio/src/operators.cpp
@@ -2,8 +2,26 @@
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Guillaume Brocker
+// Author Nicolas Papier
 
 #include "vgio/operators.hpp"
+
+#include <vgd/node/Group.hpp>
+#include <vgm/operations.hpp>
+#include <vgm/Utilities.hpp>
+
+
+
+namespace vgio
+{
+
+void configure( std::ostream & os )
+{
+	os << std::boolalpha;
+	os.precision(3);
+}
+
+}
 
 
 
@@ -49,24 +67,159 @@ std::ostream & operator << ( std::ostream & os, const vgd::node::Material::Shini
 
 
 
+std::ostream & operator << ( std::ostream & os, const vgd::Shp< vgd::node::Group > & node )
+{
+	os << node->getName();
+
+	return os;
+}
+
+
+
+std::ostream & operator << ( std::ostream & os, const vgd::Shp< vgd::node::Node > & node )
+{
+	os << node->getName();
+
+	return os;
+}
+
+
+
+
+
+
+std::ostream & operator << ( std::ostream & os, const vgm::Box3f & box )
+{
+	if ( box.isInvalid() )
+	{
+		os << "invalid";
+	}
+	else if ( box.isEmpty() )
+	{
+		os << "empty";
+	}
+	else
+	{
+		os << "min: "		<< box.getMin() << std::endl;
+		os << "max: "		<< box.getMax() << std::endl;
+		os << "center: "	<< box.getCenter() << std::endl;
+		os << "size: "		<< box.getSize();
+	}
+
+	return os;
+}
+
+
+
+std::ostream & operator << ( std::ostream & os, const vgm::XfBox3f & box )
+{
+	if ( box.isInvalid() )
+	{
+		os << "invalid";
+	}
+	else if ( box.isEmpty() )
+	{
+		os << "empty";
+	}
+	else
+	{
+		// Projected box
+		const vgm::Box3f	projectBox = box.project();
+
+		os << "projectedBox {" << projectBox << "}" << std::endl;
+
+		// Details on XfBox3f
+		// box
+		vgm::Vec3f min, max;
+		box.getBounds( min, max );
+
+		vgm::Box3f myBox( min, max );
+		os << std::endl << "box {" << myBox << "}" << std::endl;
+
+		// transformation
+		const vgm::MatrixR& myMatrix = box.getTransform();
+		os << std::endl << "transf: " << myMatrix;
+	}
+
+	return os;
+}
+
+
+
 std::ostream & operator << ( std::ostream & os, const vgm::MatrixR & matrix )
 {
-	os << "{";
-	for( uint i = 0; i < 4; ++i )
+	if ( matrix.isInvalid() )
 	{
-		for( uint j = 0; j < 4; ++j )
+		os << "invalid";
+	}
+	else if ( matrix.isIdentity() )
+	{
+		os << "identity";
+	}
+	else
+	{
+		os << "{";
+		for( uint i = 0; i < 4; ++i )
 		{
-			os << matrix(i, j);
-			if( j != 4-1 )
+			for( uint j = 0; j < 4; ++j )
 			{
-				os << ", ";
+				os << matrix(i, j);
+				if( j != 4-1 )
+				{
+					os << ", ";
+				}
+			}
+			if( i != 4-1 )
+			{
+				os << std::endl;
 			}
 		}
-		if( i != 4-1 )
-		{
-			os << std::endl;
-		}
+		os << "}";
 	}
-	os << "}";
+
+	return os;
+}
+
+
+
+std::ostream & operator << ( std::ostream & os, const vgm::Rotation & rotation )
+{
+	const vgm::Rotation identity = vgm::Rotation::getIdentity();
+
+	if ( rotation.equals( identity, vgm::Epsilon< float >::value() ) )
+	{
+		os << "identity";
+	}
+	else
+	{
+		// Retrieves axis and degree
+		vgm::Vec3f	axis;
+		float		radians;
+
+		rotation.getValue( axis, radians);
+
+		const float degrees = vgm::Utilities::rad2deg(radians);
+
+		os << "( (" << axis[0] << ", " << axis[1] << ", " << axis[2] << "), " << degrees << ")";
+	}
+
+	return os;
+}
+
+
+
+std::ostream & operator << ( std::ostream & os, const int8 & integer )
+{
+	os << static_cast<int>(integer);
+
+	return os;
+}
+
+
+
+std::ostream & operator << ( std::ostream & os, const uint8 & integer )
+{
+	os << static_cast<uint>(integer);
+
 	return os;
 }
