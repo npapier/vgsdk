4372d58163e1d67be0192409471811810f3c8551
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-06-05 08:11:58 +0000

    + Adds vgLogError2(), vgLogWarning2() and vgLogDebug2() macros.

--- core/vgDebug/include/vgDebug/convenience.hpp
+++ core/vgDebug/include/vgDebug/convenience.hpp
@@ -11,8 +11,11 @@
 ///@todo Disables some log function in debug
 ///@todo Completes vgX mapping to vgDebug::get().X()
 
-#define vgLogError( text )		vgDebug::get().logError( (text) )
-#define vgLogWarning( text )	vgDebug::get().logWarning( (text) )
-#define vgLogDebug( text )		vgDebug::get().logDebug( (text) )
+#define vgLogError( arg1 )				vgDebug::get().logError( (arg1) )
+#define vgLogError2( arg1, arg2 )		vgDebug::get().logError( (arg1), (arg2) )
+#define vgLogWarning( arg1 )			vgDebug::get().logWarning( (arg1) )
+#define vgLogWarning2( arg1, arg2 )		vgDebug::get().logWarning( (arg1), (arg2) )
+#define vgLogDebug( arg1 )				vgDebug::get().logDebug( (arg1) )
+#define vgLogDebug2( arg1, arg2 )		vgDebug::get().logDebug( (arg1), (arg2) )
 
 #endif //#ifndef _VGDEBUG_CONVENIENCE_HPP
