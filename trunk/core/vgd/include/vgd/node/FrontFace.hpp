4085f13f8991b2a82090e9d5648fed99723eb6b4
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-06-17 10:51:36 +0000

    + Adds containers.hpp to include all containers at once (TSingleField, TMultiField, TSingleAssociativeField and TPairAssociativeField). ~ Adapts Enum.hpp to new field conventions. ~ In vgd::node::FrontFace, changes type of field named orientation (from PAFBool to SFEnum).

--- trunk/core/vgd/include/vgd/node/FrontFace.hpp
+++ trunk/core/vgd/include/vgd/node/FrontFace.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004, Nicolas Papier.
+// VGSDK - Copyright (C) 2008, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -6,9 +6,7 @@
 #ifndef _VGD_NODE_FRONTFACE_HPP
 #define _VGD_NODE_FRONTFACE_HPP
 
-#include "vgd/vgd.hpp"
-
-#include "vgd/field/TPairAssociativeField.hpp"
+#include "vgd/field/Enum.hpp"
 #include "vgd/node/SingleAttribute.hpp"
 
 
@@ -23,15 +21,17 @@ namespace node
 
 /**
  * @brief Front face polygons definition node
- * 
- * The FrontFace node specifies if polygons are in clockwise winding in window coordinates, or counterclockwise winding 
- * in window coordinates, are taken to be front-facing.
- * 
- * New field added by this node :
- * 
- * - PAFBool \c [orientation] = CCW\n
- * 	 Specifies if front facing polygons are clockwise (CW) or conterclockwise (CCW).
- * 
+ *
+ * The FrontFace node specifies if polygons are in clockwise winding in window coordinates, or counterclockwise winding in window coordinates, are taken to be front-facing.
+ *
+ * New fields defined by this node :
+ * - SFEnum \c orientation = CCW
+
+ * 	Specifies if front facing polygons are clockwise (CW) or conterclockwise (CCW)
+ *
+ * Inherited fields :
+ * @todo generates this section
+ *
  * @ingroup g_nodes
  * @ingroup g_singleAttributeNodes
  * @ingroup g_propertiesNodes
@@ -43,85 +43,74 @@ struct VGD_API FrontFace : public vgd::node::SingleAttribute
 
 
 	/**
-	 * @name Accessors to field orientation.
+	 * @name Accessors to field \c orientation
 	 */
 	//@{
 
 	/**
-	 * @brief Enumeration of the \c orientation parameter.
+	 * @brief Type definition of the value contained by field named \c orientation.
 	 */
 	typedef enum
 	{
-		ORIENTATION = 1
-	} OrientationParameterType;
-
-	/**
-	 * @brief Typedef for the \c orientation parameter value.
-	 */
-	typedef enum
-	{
-		CCW = 1,
-		CW,
-		DEFAULT_ORIENTATION = CCW
+		CCW = 256,	///< (C)ounter-(C)lock(W)ise ordering
+		CW = 257,	///< (C)lock(W)ise ordering
+		DEFAULT_ORIENTATION = CCW	///< (C)ounter-(C)lock(W)ise ordering
 	} OrientationValueType;
 
 	/**
-	 * @brief Typedef for the \c orientation field.
-	 */	
-	typedef vgd::field::TPairAssociativeField< OrientationParameterType, OrientationValueType > FOrientationType;
+	 * @brief Type definition of the field named \c orientation
+	 */
+	typedef vgd::field::TSingleField< OrientationValueType > FOrientationType;
 
 	/**
-	 * @brief Gets the \c orientation value.
+	 * @brief Gets the value of field named \c orientation.
 	 */
-	bool			getOrientation( OrientationValueType& value ) const;
+	const OrientationValueType getOrientation() const;
 
 	/**
-	 * @brief Sets the \c orientation value.
+	 * @brief Sets the value of field named \c orientation.
 	 */
-	void 			setOrientation( OrientationValueType value );
-	
-	/**
-	 * @brief Erase the \c orientation value.
-	 */
-	void 			eraseOrientation();
+	void setOrientation( const OrientationValueType value );
+
 	//@}
 
 
 
 	/**
-	 * @name Fields names enumeration.
+	 * @name Field name accessors
 	 */
 	//@{
-	
+
 	/**
 	 * @brief Returns the name of field \c orientation.
 	 * 
 	 * @return the name of field \c orientation.
 	 */
 	static const std::string getFOrientation( void );
+
 	//@}
 
-	
-	
-protected:
+
+
 	/**
-	 * @name Constructor.
+	 * @name Constructor and initializer methods
 	 */
 	//@{
 
+protected:
 	/**
-	 * @brief Default constructor.
-	 * 
-	 * @internal Add field, dirty flags and link in the constructor.
+	 * @brief Default constructor
 	 */
 	FrontFace( const std::string nodeName );
 
+public:
 	void	setToDefaults( void );
 
 	void	setOptionalsToDefaults();
-	
+
 	//@}
 };
+
 
 
 } // namespace node
--- trunk/engines/vgeGL/src/handler/painter/FrontFace.cpp
+++ trunk/engines/vgeGL/src/handler/painter/FrontFace.cpp
@@ -1,17 +1,11 @@
-// VGSDK - Copyright (C) 2004, Nicolas Papier.
+// VGSDK - Copyright (C) 2004, 2008, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
 
 #include "vgeGL/handler/painter/FrontFace.hpp"
 
-#include <vgd/field/DirtyFlag.hpp>
 #include <vgd/node/FrontFace.hpp>
-#include <vge/rc/Manager.hpp>
-#include <vge/service/Painter.hpp>
-
-#include "vgeGL/engine/Engine.hpp"
-
 #include "vgeGL/rc/TDisplayListHelper.hpp"
 
 
@@ -34,11 +28,11 @@ META_HANDLER_CPP( FrontFace );
 const vge::handler::Handler::TargetVector FrontFace::getTargets() const
 {
 	TargetVector targets;
-	
+
 	targets.reserve( 1 );
 	targets.push_back( vgd::node::FrontFace::getClassIndexStatic() );
 
-	return ( targets );
+	return targets;
 }
 
 
@@ -50,7 +44,7 @@ void FrontFace::apply ( vge::engine::Engine* pEngine, vgd::node::Node *pNode )
 
 //	assert( dynamic_cast< vgd::node::FrontFace* >(pNode) != 0 );
 //	vgd::node::FrontFace *pCastedNode = static_cast< vgd::node::FrontFace* >(pNode);
-	
+
 	vgeGL::rc::applyUsingDisplayList< vgd::node::FrontFace, FrontFace >( pEngine, pNode, this );
 }
 
@@ -68,21 +62,15 @@ void FrontFace::setToDefaults()
 
 
 
-void FrontFace::paint( vgeGL::engine::Engine *, vgd::node::FrontFace *pNode )
+void FrontFace::paint( vgeGL::engine::Engine *, vgd::node::FrontFace * node )
 {
 	// ORIENTATION
-	bool														bDefined;
-	vgd::node::FrontFace::OrientationValueType	value;
+	vgd::node::FrontFace::OrientationValueType value = node->getOrientation();
 
-	bDefined = pNode->getOrientation( value );
+	glFrontFace( (value == vgd::node::FrontFace::CCW) ? GL_CCW : GL_CW );
 
-	if ( bDefined )
-	{
-		glFrontFace( (value == vgd::node::FrontFace::CCW) ? GL_CCW : GL_CW );
-	}
-	
-	// Validate node
-	pNode->getDirtyFlag(pNode->getDFNode())->validate();		
+	// Validates node df
+	node->getDirtyFlag(node->getDFNode())->validate();
 }
 
 
