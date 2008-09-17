a006b631ce33cb8fb15c09b55ec2d2b3b4f7bc09
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-09-17 15:43:33 +0000

    ~ Adds dirty flag validation for Group, Switch and MultiSwitch nodes.

--- trunk/engines/vge/src/handler/painter/Group.cpp
+++ trunk/engines/vge/src/handler/painter/Group.cpp
@@ -1,6 +1,35 @@
-// VGSDK - Copyright (C) 2004, Nicolas Papier.
+// VGSDK - Copyright (C) 2004, 2008, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
 
 #include "vge/handler/painter/Group.hpp"
+
+#include <vgd/node/Node.hpp>
+
+
+
+namespace vge
+{
+
+namespace handler
+{
+
+namespace painter
+{
+
+
+
+void Group::apply( vge::engine::Engine * /*engine*/, vgd::node::Node * node )
+{
+	// Validates node
+	node->getDirtyFlag(node->getDFNode())->validate();
+}
+
+
+
+} // namespace painter
+
+} // namespace handler
+
+} // namespace vge
