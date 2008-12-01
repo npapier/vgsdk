14ddefa65f97010f6f354c4160f7360b2e4497f9
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2008-12-01 10:31:20 +0000

    ~ Updates the 'Settings' page by adding several buttons to show counters, log events and enable capture hotkeys.

--- trunk/ui/vgGTK/src/engine/Settings.cpp
+++ trunk/ui/vgGTK/src/engine/Settings.cpp
@@ -9,6 +9,7 @@
 #include <gtkmm/button.h>
 #include <gtkmm/checkbutton.h>
 #include <gtkmm/separator.h>
+#include <vgUI/event/CaptureEventProcessor.hpp>
 #include <vgeGL/engine/Engine.hpp>
 #include <vgUI/Canvas.hpp>
 
@@ -28,20 +29,40 @@ Settings::Settings()
 {
 	// Creates child widgets.
 	m_glslButton								= Gtk::manage( new Gtk::CheckButton("Use GLSL pipeline") );
+	m_disableTexture							= Gtk::manage( new Gtk::CheckButton("Disable texture (todo)") );
 	Gtk::Button		* benchButton				= Gtk::manage( new Gtk::Button("Bench") );
+
+	m_showFPS									= Gtk::manage( new Gtk::CheckButton("Show counters (fps and frame)") );
+	m_debugEvents								= Gtk::manage( new Gtk::CheckButton("Log events") );
 	Gtk::Button		* clearGLResourcesButton	= Gtk::manage( new Gtk::Button("Clear OpenGL Resources") );
-	
+
+	m_captureButton								= Gtk::manage( new Gtk::CheckButton("Enable capture HotKeys") );
+
 	set_spacing( 8 );
+
 	pack_start( *m_glslButton, Gtk::PACK_SHRINK );
+	pack_start( *m_disableTexture, Gtk::PACK_SHRINK );
+	pack_start( *m_showFPS, Gtk::PACK_SHRINK );
+	pack_start( *m_debugEvents, Gtk::PACK_SHRINK );
+
 	pack_start( *Gtk::manage(new Gtk::HSeparator()), Gtk::PACK_SHRINK );
 	pack_start( *benchButton, Gtk::PACK_SHRINK );
 	pack_start( *clearGLResourcesButton, Gtk::PACK_SHRINK );
 
+	pack_start( *Gtk::manage(new Gtk::HSeparator()), Gtk::PACK_SHRINK );
+	pack_start( *m_captureButton, Gtk::PACK_SHRINK );
 
 	// Connects signal handlers.
-	clearGLResourcesButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onClearGLResources) );
 	m_glslButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onGLSL) );
+	m_disableTexture->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableTexture) );
+
 	benchButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onBench) );
+
+	m_showFPS->signal_clicked().connect( sigc::mem_fun(this, &Settings::onShowFPS) );
+	m_debugEvents->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDebugEvents) );
+	clearGLResourcesButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onClearGLResources) );
+
+	m_captureButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onCaptureButton) );
 
 	// Refresh child sensitiveness.
 	refreshChildrenSensitiveness();
@@ -61,14 +82,63 @@ void Settings::setCanvas( vgUI::Canvas * canvas )
 
 
 
+void Settings::onGLSL()
+{
+	assert( m_canvas != 0 );
+
+	m_canvas->getGLEngine()->setGLSLEnabled( m_glslButton->get_active() );
+	m_canvas->refresh( vgUI::Canvas::REFRESH_FORCE );
+}
+
+
+
+void Settings::onDisableTexture()
+{
+	assert( m_canvas != 0 );
+
+	// @todo
+	/*m_canvas->getGLEngine()->setGLSLEnabled( m_glslButton->get_active() );
+	m_canvas->refresh( vgUI::Canvas::REFRESH_FORCE );*/
+}
+
+
+
 void Settings::onBench()
 {
 	assert( m_canvas != 0 );
 
-	m_canvas->bench(200);
-	m_canvas->refresh( vgUI::Canvas::REFRESH_FORCE, vgUI::Canvas::ASYNCHRONOUS );
+	const int fps = m_canvas->getFPS();
+
+	if ( fps >= 0 )
+	{
+		m_canvas->bench( fps * 2 );
+		m_canvas->refresh( vgUI::Canvas::REFRESH_FORCE, vgUI::Canvas::ASYNCHRONOUS );
+	}
 }
 
+
+
+
+void Settings::onShowFPS()
+{
+	assert( m_canvas != 0 );
+
+	const bool isDebugOverlay = m_showFPS->get_active();
+	m_canvas->setDebugOverlay( isDebugOverlay );
+
+	m_canvas->refresh(); // vgUI::Canvas::REFRESH_FORCE );
+}
+
+
+
+void Settings::onDebugEvents()
+{
+	assert( m_canvas != 0 );
+
+	const bool isDebugEvents = m_debugEvents->get_active();
+
+	m_canvas->setDebugEvents( isDebugEvents );
+}
 
 
 
@@ -82,12 +152,31 @@ void Settings::onClearGLResources()
 
 
 
-void Settings::onGLSL()
+void Settings::onCaptureButton()
 {
 	assert( m_canvas != 0 );
-	
-	m_canvas->getGLEngine()->setGLSLEnabled( m_glslButton->get_active() );
-	m_canvas->refresh( vgUI::Canvas::REFRESH_FORCE );
+
+	const bool enableCapture = m_captureButton->get_active();
+
+	using vgUI::event::CaptureEventProcessor;
+
+	if ( enableCapture )
+	{
+		// Inserts capture event processor
+		vgd::Shp< CaptureEventProcessor > captureEventProcessor( new CaptureEventProcessor(m_canvas) );
+		m_canvas->insertEventProcessor( captureEventProcessor );
+	}
+	else
+	{
+		// Removes capture event processor
+		const int index = m_canvas->findEventProcessor< CaptureEventProcessor >();
+		if ( index != m_canvas->getNumEventProcessors() )
+		{
+			// Capture event processor found
+			m_canvas->removeEventProcessor( index );
+		}
+		// else nothing to do
+	}
 }
 
 
