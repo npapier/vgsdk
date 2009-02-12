dcb2b7687287ba892e82ab8db16d510d99c033f1
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2009-02-12 14:08:04 +0000

    + Adds a new Vector constructor given with value affectation from array of n components (pointer on the first element of any type) and a similar method named setValue(). + Adds getAngle() in Vector to compute the angle between two vectors.

--- trunk/core/vgm/include/vgm/Vector.hpp
+++ trunk/core/vgm/include/vgm/Vector.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004, 2006, 2007, 2008, Nicolas Papier.
+// VGSDK - Copyright (C) 2004, 2006, 2007, 2008, 2009, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -55,7 +55,7 @@
 #include <limits>
 #include <vector>
 
-#include "vgm/vgm.hpp"
+#include "vgm/operations.hpp"
 
 
 
@@ -92,6 +92,12 @@ struct Vector
 	 * @brief Constructor with value affectation from array of n components.
 	 */
 	explicit Vector( const T* v );
+
+	/**
+	 * @brief Constructor with value affectation from array of n components.
+	 */
+	template< typename InType >
+	explicit Vector< T, N >( const InType* v );
 
 	/**
 	 * @brief Copy constructor.
@@ -239,6 +245,12 @@ vgm::Vec4f		out5b( in5 );
 	 * @brief Set value of vector from array of n components.
 	 */
 	void		setValue( const T* );
+
+	/**
+	 * @brief Set value of vector from array of n components.
+	 */
+	template< typename InType >
+	void		setValue( const InType* );
 
 	/**
 	 * @brief Set value of vector from another one (with same dimension).
@@ -456,6 +468,13 @@ vgm::Vec4f		out5b( in5 );
 	 * @brief Returns the dot product of two vectors.
 	 */
 	float		dot( const Vector& v ) const;
+
+	/**
+	 * @brief Returns the angle between two vectors.
+	 *
+	 * @return the angle in radians between the two vectors.
+	 */
+	const float getAngle( const Vector& v ) const;
 
 
 
