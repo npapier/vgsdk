a4354d656369a571f8bf5e197f27dae5b3051bef
guillaume.brocker <guillaume.brocker@70756234-2556-0410-8cce-5df1b8bfd00f>
2009-03-10 12:45:15 +0000

    + Adds a new dialog that allows to configure the resolution (size) of a widget. It is intended to configure the size of a rendering canvas.

--- trunk/examples/gtk/vgsdkViewerGtk/src/stock/stock.cpp
+++ trunk/examples/gtk/vgsdkViewerGtk/src/stock/stock.cpp
@@ -23,12 +23,15 @@ namespace stock
 #include "vgsdkViewerGtk/stock/multi-view-sided-24.xpm"
 #include "vgsdkViewerGtk/stock/multi-view-squared-16.xpm"
 #include "vgsdkViewerGtk/stock/multi-view-squared-24.xpm"
+#include "vgsdkViewerGtk/stock/resolution-16.xpm"
+#include "vgsdkViewerGtk/stock/resolution-24.xpm"
 
 
 
 Gtk::StockID	SINGLE_VIEW("vgsdkViewerGtk::stock::SINGLE_VIEW");
 Gtk::StockID	MULTI_VIEW_SIDED("vgsdkViewerGtk::stock::MULTI_VIEW_SIDED");
 Gtk::StockID	MULTI_VIEW_SQUARED("vgsdkViewerGtk::stock::MULTI_VIEW_SQUARED");
+Gtk::StockID	RESOLUTION("vgsdkViewerGtk::stock::RESOLUTION");
 
 
 
@@ -59,6 +62,10 @@ void initialize()
 	multiViewSquaredSet.add_source( createIconSource(16, multi_view_squared_16_xpm) );
 	multiViewSquaredSet.add_source( createIconSource(24, multi_view_squared_24_xpm) );
 	
+	Gtk::IconSet	resolutionSet;
+	resolutionSet.add_source( createIconSource(16, resolution_16_xpm) );
+	resolutionSet.add_source( createIconSource(24, resolution_24_xpm) );
+	
 	
 	// Creates an icon factory for our icon sets.
 	Glib::RefPtr< Gtk::IconFactory >	iconFactory = Gtk::IconFactory::create();
@@ -66,6 +73,7 @@ void initialize()
 	iconFactory->add( SINGLE_VIEW, singleViewSet );
 	iconFactory->add( MULTI_VIEW_SIDED, multiViewSidedSet );
 	iconFactory->add( MULTI_VIEW_SQUARED, multiViewSquaredSet );
+	iconFactory->add( RESOLUTION, resolutionSet );
 	iconFactory->add_default();
 }
 
