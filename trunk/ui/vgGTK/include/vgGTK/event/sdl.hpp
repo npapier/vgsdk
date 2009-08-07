611e24743ab522674fb1e4a42d8a08503f3dae92
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2009-08-07 13:09:51 +0000

    ~ Adds support of several new keys (left, up, right and down) in vgGTK::event::getKeyboardButton().

--- trunk/ui/vgGTK/include/vgGTK/event/sdl.hpp
+++ trunk/ui/vgGTK/include/vgGTK/event/sdl.hpp
@@ -1,3 +1,8 @@
+// VGSDK - Copyright (C) 2009, Clement Forest.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Clement Forest
+
 #ifndef _VGGTK_EVENT_SDL_HPP_
 #define _VGGTK_EVENT_SDL_HPP_
 
@@ -9,13 +14,14 @@ namespace event
 {
 
 /**
- * @brief Init SDL managment.
+ * @brief Initialization of SDL management.
  *
- * Init SDL and start its managment by GTK.
+ * Initializes SDL and start its managment by GTK.
  */
 void VGGTK_API initSDL();
 
 } // namespace event
+
 } // namespace vgGTK
 
 #endif // _VGGTK_EVENT_SDLEVENT_HPP_
