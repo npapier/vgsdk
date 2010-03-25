23c1e648dee3df8971dda4e1c042df86ee974ebc
podidoo <podidoo@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-03-25 11:12:10 +0000

    + Adds accessors to enable popup menu (see ActionNode).

--- trunk/ui/vgGTK/include/vgGTK/node/ActionsNode.hpp
+++ trunk/ui/vgGTK/include/vgGTK/node/ActionsNode.hpp
@@ -40,6 +40,8 @@ struct VGGTK_API ActionsNode
 	void setCanvas( vgUI::Canvas * canvas );
 	void setBrowser( vgGTK::graph::Browser * browser );
 
+	bool onBoutonPressEvent(GdkEventButton * event );
+
 /**
  * @brief Show the popup menu
  * 
