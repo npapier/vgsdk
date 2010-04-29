3bde9e27469b3742f93ea6ca0bb5356303889ea0
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-04-29 12:22:15 +0000

    + Adds equality comparisons within tolerance for float and double.

--- core/vgm/src/operations.cpp
+++ core/vgm/src/operations.cpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2006, Nicolas Papier.
+// VGSDK - Copyright (C) 2006, 2010, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -12,18 +12,49 @@
 namespace vgm
 {
 
+
+const bool equals( const float value1, const float value2 )
+{
+	const float diff = fabs( value1 - value2 );
+
+	return diff <= vgm::Epsilon<float>::value();
+}
+
+
+const bool notEquals( const float value1, const float value2 )
+{
+	const float diff = fabs( value1 - value2 );
+
+	return diff > vgm::Epsilon<float>::value();
+}
+
+
+const bool equals( const double value1, const double value2 )
+{
+	const double diff = fabs( value1 - value2 );
+
+	return diff <= vgm::Epsilon<double>::value();
+}
+
+
+const bool notEquals( const double value1, const double value2 )
+{
+	const double diff = fabs( value1 - value2 );
+
+	return diff > vgm::Epsilon<double>::value();
+}
+
+
 const double round( const double inputValue )
 {
 	return floor(inputValue + 0.5f);
 }
 
 
-
 const float roundf( const float inputValue )
 {
 	return floorf(inputValue + 0.5f);
 }
-
 
 
 } // namespace vgm
