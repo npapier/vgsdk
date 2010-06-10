23360176fd5d370455887d26237dadc34f62dcbd
podidoo <podidoo@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-06-10 10:20:16 +0000

    + Adds full multiple instances support for COLLADA import/export. + Adds texture matrix transform support for COLLADA import/export. ~ Fixes stack trace reset problem (reset now called in prepareEval() method). ~ Fixes some material import/export issues.

---
+++ io/vgOpenCOLLADA/src/exporter/InstanceGeometry.cpp
@@ -0,0 +1,68 @@
+// VGSDK - Copyright (C) 2010 Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Maxime Peresson
+
+#include "vgOpenCOLLADA/exporter/InstanceGeometry.hpp"
+
+
+namespace vgOpenCOLLADA
+{
+
+namespace exporter
+{
+
+InstanceGeometry::InstanceGeometry( COLLADASW::StreamWriter * streamWriter )
+:	COLLADASW::InstanceGeometry ( streamWriter ),
+	m_name( "" ),
+	m_matrixCounter( 0 )
+{
+}
+
+
+
+void InstanceGeometry::addMultipleInstanceMatrix( vgm::MatrixR matrix )
+{
+	double n[4][4];
+	std::stringstream ss;
+
+	ss << "<matrix>";
+	for (size_t iRow = 0 ;iRow < 4 ; iRow++) 
+	{
+		for (size_t iCol = 0; iCol < 4; iCol++) 
+		{
+			ss <<  matrix[iCol][iRow] << " ";
+		}
+	}
+
+	ss << "</matrix>" << std::endl;
+
+	m_multipleInstancesText += ss.str();
+	m_matrixCounter++;
+}
+
+
+
+void InstanceGeometry::createMultipleInstances()
+{
+	m_extraTechnique.addExtraTechniqueTextblock( "profile_vgsdk", m_multipleInstancesText );
+	
+	m_extraTechnique.addExtraTechniques( mSW );
+}
+
+
+
+void InstanceGeometry::setName( std::string name )
+{
+	m_name = name;
+	std::stringstream ss;
+	ss << "<name>" << m_name << "</name>" << std::endl;
+	m_multipleInstancesText += ss.str();
+
+}
+
+
+
+} // namespace exporter
+
+} // namespace vgOpenCOLLADA
