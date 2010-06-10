0858cbf2c11de5c799bac0b32e140b8c6a9c0820
podidoo <podidoo@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-06-10 10:20:16 +0000

    + Adds full multiple instances support for COLLADA import/export. + Adds texture matrix transform support for COLLADA import/export. ~ Fixes stack trace reset problem (reset now called in prepareEval() method). ~ Fixes some material import/export issues.

---
+++ trunk/io/vgOpenCOLLADA/include/vgOpenCOLLADA/exporter/InstanceGeometry.hpp
@@ -0,0 +1,46 @@
+// VGSDK - Copyright (C) 2010 Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Maxime Peresson
+
+#ifndef _VGOPENCOLLADA_EXPORTER_INSTANCEGEOMETRY_HPP
+#define _VGOPENCOLLADA_EXPORTER_INSTANCEGEOMETRY_HPP
+
+#include <COLLADASWInstanceGeometry.h>
+#include <COLLADASWExtraTechnique.h>
+
+#include <vgm/Matrix.hpp>
+
+
+namespace vgOpenCOLLADA
+{
+
+namespace exporter
+{
+
+/**
+ * @brief	COLLADA instance geometry.
+ *			Adds vgSDK MultiInstances support.
+ */
+struct InstanceGeometry : public COLLADASW::InstanceGeometry
+{
+
+	InstanceGeometry( COLLADASW::StreamWriter * streamWriter );
+
+	void setName( std::string name );
+	void addMultipleInstanceMatrix( vgm::MatrixR matrix );
+	void createMultipleInstances();
+
+private:
+	COLLADASW::BaseExtraTechnique	m_extraTechnique;
+	std::string						m_name;
+	int								m_matrixCounter;
+	std::string						m_multipleInstancesText;
+
+};
+
+} // namespace exporter
+
+} // namespace vgOpenCOLLADA
+
+#endif // _VGOPENCOLLADA_EXPORTER_INSTANCEGEOMETRY_HPP
