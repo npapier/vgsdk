e7940c122e236207c3b7dac078efa9afe74fc0de
Clement.Forest <Clement.Forest@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-09-07 07:31:09 +0000

    ~ Removes warnings

--- io/vgCollada/src/domSourceAdapter.cpp
+++ io/vgCollada/src/domSourceAdapter.cpp
@@ -37,7 +37,7 @@ domSourceAdapter::domSourceAdapter( domSource * source )
 	domAccessor * accessor = technique->getAccessor();
 
 	// Initializes m_arrayType
-	const uint stride = accessor->getStride();
+	const uint stride = (uint)accessor->getStride();
 	switch ( stride )
 	{
 		case 1:
@@ -57,7 +57,7 @@ domSourceAdapter::domSourceAdapter( domSource * source )
 			return;
 	}
 
-	const uint count = accessor->getCount();
+	const uint count = (uint)accessor->getCount();
 
 	// Fills array
 	daeDoubleArray * float_array = 0;
@@ -78,33 +78,33 @@ domSourceAdapter::domSourceAdapter( domSource * source )
 		case 1:
 			for( size_t i = 0; i < count; ++i )
 			{
-				m_float.push_back( va[i] );
+				m_float.push_back( (float)va[i] );
 			}
 			break;
 
 		case 2:
 			for( size_t i = 0; i < count; ++i )
 			{
-				m_vec2f.push_back( vgm::Vec2f(va[i*2], va[i*2+1]) );
+				m_vec2f.push_back( vgm::Vec2f((float)va[i*2], (float)va[i*2+1]) );
 			}
 			break;
 
 		case 3:
 			for( size_t i = 0; i < count; ++i )
 			{
-				m_vec3f.push_back( vgm::Vec3f(va[i*3], va[i*3+1], va[i*3+2]) );
+				m_vec3f.push_back( vgm::Vec3f((float)va[i*3], (float)va[i*3+1], (float)va[i*3+2]) );
 			}
 			break;
 
 		case 4:
 			for( size_t i = 0; i < count; ++i )
 			{
-				m_vec4f.push_back( vgm::Vec4f(va[i*4], va[i*4+1], va[i*4+2], va[i*4+3]) );
+				m_vec4f.push_back( vgm::Vec4f((float)va[i*4], (float)va[i*4+1], (float)va[i*4+2], (float)va[i*4+3]) );
 			}
 			break;
 
 		default:
-			vgLogDebug("Unsupported stride %i.", stride);
+			vgLogDebug2("Unsupported stride %i.", stride);
 	}
 }
 
