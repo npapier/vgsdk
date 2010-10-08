0ac56afbcb4edb30587efbc1d211e1f5023a209f
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2010-10-08 08:25:07 +0000

    ~ vgBase is now a shared library.

---
+++ trunk/core/vgBase/src/vgBase.cpp
@@ -0,0 +1,16 @@
+// VGSDK - Copyright (C) 2010, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include "vgBase/vgBase.hpp"
+
+
+
+namespace vgBase
+{
+
+void vgBase()
+{}
+
+} // namespace vgBase
