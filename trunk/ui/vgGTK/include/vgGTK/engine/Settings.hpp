ece00124bf4aea0e54ac368631bdff76310f8087
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-04-19 12:43:57 +0000

    + Lighting, shadow and depth pre-pass could be disabled at engine level and/or using a gtkmm gui.

--- trunk/ui/vgGTK/include/vgGTK/engine/Settings.hpp
+++ trunk/ui/vgGTK/include/vgGTK/engine/Settings.hpp
@@ -2,6 +2,7 @@
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Guillaume Brocker
+// Author Nicolas Papier
 
 #ifndef VGGTK_ENGINE_SETTINGS_HPP
 #define VGGTK_ENGINE_SETTINGS_HPP
@@ -33,6 +34,7 @@ namespace engine
 /**
  * @brief	A widget allowing to configure rendering engine's settings.
  */
+GTKMM_BEGIN_CLASS()
 struct VGGTK_API Settings : public Gtk::VBox
 {
 	/**
@@ -58,9 +60,12 @@ private:
 	//@{
 	void onGLSL();
 
+	void onDisableLighting();
 	void onDisableTexture();
+	void onDisableShadow();
 	void onDisableDisplayList();
 	void onDisableVBO();
+	void onDisableDepthPrePass();
 
 	void onBench();
 
@@ -77,13 +82,20 @@ private:
 	void refreshChildrenSensitiveness();
 
 	Gtk::CheckButton	* m_glslButton;			///< Button to enable/disable glsl rendering pipeline
-	Gtk::CheckButton	* m_disableTexture;		///< Button to disable texture
-	Gtk::CheckButton	* m_disableDisplayList;	///< Button to disable display list
-	Gtk::CheckButton	* m_disableVBO;			///< Button to disable VBO
+
+	Gtk::CheckButton	* m_disableLighting;	///< Button to disable lighting in engine
+	Gtk::CheckButton	* m_disableTexture;		///< Button to disable texture in engine
+	Gtk::CheckButton	* m_disableShadow;		///< Button to disable shadow in engine
+
+	Gtk::CheckButton	* m_disableDisplayList;	///< Button to disable display list in engine
+	Gtk::CheckButton	* m_disableVBO;			///< Button to disable VBO in engine
+	Gtk::CheckButton	* m_disableDepthPrePass;///< Button to disable depth pre-pass in engine
+
 	Gtk::CheckButton	* m_showFPS;			///< Widget to choose which debug overlay must be displayed
 	Gtk::CheckButton	* m_debugEvents;		///< Button to enable/disable event logging
 	Gtk::CheckButton	* m_captureButton;		///< Widget to enable screenshot/video capture hotkey
 };
+GTKMM_END_CLASS()
 
 
 
