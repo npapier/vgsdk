a39cbe131bccdec0735d0f01e6abc36aeb9117dc
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-04-19 12:43:57 +0000

    + Lighting, shadow and depth pre-pass could be disabled at engine level and/or using a gtkmm gui.

--- ui/vgGTK/src/engine/Settings.cpp
+++ ui/vgGTK/src/engine/Settings.cpp
@@ -2,6 +2,7 @@
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Guillaume Brocker
+// Author Nicolas Papier
 
 #include "vgGTK/engine/Settings.hpp"
 
@@ -25,14 +26,45 @@ namespace engine
 
 
 
+namespace
+{
+
+
+void dirtyAllNodes( vgUI::Canvas * canvas )
+{
+	vgd::Shp< vgd::node::NodeList > nodeList;
+	nodeList = vgd::visitor::find( canvas->getRoot(), vgd::visitor::predicate::True() );
+
+	using vgd::node::NodeList;
+
+	for( NodeList::const_iterator	i		= nodeList->begin(),
+									iEnd	= nodeList->end();
+		i != iEnd;
+		++i )
+	{
+		vgd::Shp< vgd::node::Node > node = *i;
+		node->getDirtyFlag( node->getDFNode() )->dirty();
+	}
+}
+
+
+}
+
+
+
 Settings::Settings()
 :	m_canvas( 0 )
 {
 	// Creates child widgets.
 	m_glslButton								= Gtk::manage( new Gtk::CheckButton("Use GLSL pipeline") );
+
+	m_disableLighting							= Gtk::manage( new Gtk::CheckButton("Disable lighting") );
 	m_disableTexture							= Gtk::manage( new Gtk::CheckButton("Disable texture") );
+	m_disableShadow								= Gtk::manage( new Gtk::CheckButton("Disable shadow") );
 	m_disableDisplayList						= Gtk::manage( new Gtk::CheckButton("Disable display list") );
 	m_disableVBO								= Gtk::manage( new Gtk::CheckButton("Disable VBO") );
+	m_disableDepthPrePass						= Gtk::manage( new Gtk::CheckButton("Disable Depth pre-pass") );
+
 	Gtk::Button		* benchButton				= Gtk::manage( new Gtk::Button("Bench") );
 
 	m_showFPS									= Gtk::manage( new Gtk::CheckButton("Show counters (fps and frame)") );
@@ -45,9 +77,16 @@ Settings::Settings()
 	set_spacing( 8 );
 
 	pack_start( *m_glslButton, Gtk::PACK_SHRINK );
+	pack_start( *Gtk::manage(new Gtk::HSeparator()), Gtk::PACK_SHRINK );
+
+	pack_start( *m_disableLighting, Gtk::PACK_SHRINK );
 	pack_start( *m_disableTexture, Gtk::PACK_SHRINK );
+	pack_start( *m_disableShadow, Gtk::PACK_SHRINK );
+	pack_start( *Gtk::manage(new Gtk::HSeparator()), Gtk::PACK_SHRINK );
+
 	pack_start( *m_disableDisplayList, Gtk::PACK_SHRINK );
 	pack_start( *m_disableVBO, Gtk::PACK_SHRINK );
+	pack_start( *m_disableDepthPrePass, Gtk::PACK_SHRINK );
 
 	pack_start( *Gtk::manage(new Gtk::HSeparator()), Gtk::PACK_SHRINK );
 	pack_start( *m_showFPS, Gtk::PACK_SHRINK );
@@ -62,9 +101,12 @@ Settings::Settings()
 
 	// Connects signal handlers.
 	m_glslButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onGLSL) );
+	m_disableLighting->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableLighting) );
 	m_disableTexture->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableTexture) );
+	m_disableShadow->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableShadow ) );
 	m_disableDisplayList->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableDisplayList ) );
 	m_disableVBO->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableVBO ) );
+	m_disableDepthPrePass->signal_clicked().connect( sigc::mem_fun(this, &Settings::onDisableDepthPrePass ) );
 
 	benchButton->signal_clicked().connect( sigc::mem_fun(this, &Settings::onBench) );
 
@@ -90,9 +132,14 @@ void Settings::setCanvas( vgUI::Canvas * canvas )
 	if( m_canvas != 0 )
 	{
 		m_glslButton->set_active( m_canvas->getGLEngine()->isGLSLEnabled() );
+
+		m_disableLighting->set_active( !m_canvas->getGLEngine()->isLightingEnabled() );
 		m_disableTexture->set_active( !m_canvas->getGLEngine()->isTextureMappingEnabled() );
+		m_disableShadow->set_active( !m_canvas->getGLEngine()->isShadowEnabled() );
 		m_disableDisplayList->set_active( !m_canvas->getGLEngine()->isDisplayListEnabled() );
 		m_disableVBO->set_active( !m_canvas->getGLEngine()->isVertexBufferObjectEnabled() );
+		m_disableDepthPrePass->set_active( !m_canvas->getGLEngine()->isDepthPrePassEnabled() );
+
 		m_showFPS->set_active( m_canvas->isDebugOverlay() );
 	}
 }
@@ -109,26 +156,36 @@ void Settings::onGLSL()
 
 
 
+void Settings::onDisableLighting()
+{
+	assert( m_canvas != 0 );
+	m_canvas->getGLEngine()->setLightingEnabled( !m_disableLighting->get_active() );
+
+	dirtyAllNodes( m_canvas );
+
+	m_canvas->refreshForced();
+}
+
+
+
 void Settings::onDisableTexture()
 {
 	assert( m_canvas != 0 );
 	m_canvas->getGLEngine()->setTextureMappingEnabled( !m_disableTexture->get_active() );
 
-	// @todo a function to dirty all nodeDF
-	vgd::Shp< vgd::node::NodeList > nodeList;
-	nodeList = vgd::visitor::find( m_canvas->getRoot(), vgd::visitor::predicate::True() );
+	dirtyAllNodes( m_canvas );
 
-	using vgd::node::NodeList;
+	m_canvas->refreshForced();
+}
 
-	for( NodeList::const_iterator	i		= nodeList->begin(),
-									iEnd	= nodeList->end();
-		i != iEnd;
-		++i )
-	{
-		vgd::Shp< vgd::node::Node > node = *i;
-		node->getDirtyFlag( node->getDFNode() )->dirty();
-	}
-	// end todo
+
+
+void Settings::onDisableShadow()
+{
+	assert( m_canvas != 0 );
+	m_canvas->getGLEngine()->setShadowEnabled( !m_disableShadow->get_active() );
+
+	dirtyAllNodes( m_canvas );
 
 	m_canvas->refreshForced();
 }
@@ -141,33 +198,7 @@ void Settings::onDisableDisplayList()
 
 	m_canvas->getGLEngine()->setDisplayListEnabled( !m_disableDisplayList->get_active() );
 
-	typedef vge::visitor::NodeCollectorExtended<> NodeCollectorExtended;
-	NodeCollectorExtended nodeCollector;
-	m_canvas->getRoot()->traverse( nodeCollector );
-
-	typedef vge::visitor::TraverseElementVector TraverseElementVector;
-	TraverseElementVector * traverseElements = nodeCollector.getTraverseElements();
-
-	TraverseElementVector::iterator i, iEnd;
-	for(	i = traverseElements->begin(), iEnd = traverseElements->end();
-			i != iEnd;
-			++i )
-	{
-		vgd::node::Node * node = i->first;
-		node->getDirtyFlag( node->getDFNode() )->dirty();
-	}
-	/*vgd::Shp< vgd::node::NodeList > nodeList;
-	nodeList = vgd::visitor::find( m_canvas->getRoot(), vgd::visitor::predicate::True() );
-	using vgd::node::NodeList;
-
-	for( NodeList::const_iterator	i		= nodeList->begin(),
-									iEnd	= nodeList->end();
-		i != iEnd;
-		++i )
-	{
-		vgd::Shp< vgd::node::Node > node = *i;
-		node->getDirtyFlag( node->getDFNode() )->dirty();
-	}*/
+	dirtyAllNodes( m_canvas );
 
 	m_canvas->refreshForced();
 }
@@ -179,23 +210,22 @@ void Settings::onDisableVBO()
 	assert( m_canvas != 0 );
 
 	m_canvas->getGLEngine()->setVertexBufferObjectEnabled( !m_disableVBO->get_active() );
-	typedef vge::visitor::NodeCollectorExtended<> NodeCollectorExtended;
-	NodeCollectorExtended nodeCollector;
-	m_canvas->getRoot()->traverse( nodeCollector );
 
-	typedef vge::visitor::TraverseElementVector TraverseElementVector;
-	TraverseElementVector * traverseElements = nodeCollector.getTraverseElements();
+	dirtyAllNodes( m_canvas );
 
-	TraverseElementVector::iterator i, iEnd;
-	for(	i = traverseElements->begin(), iEnd = traverseElements->end();
-			i != iEnd;
-			++i )
-	{
-		vgd::node::Node * node = i->first;
-		node->getDirtyFlag( node->getDFNode() )->dirty();
-	}
-	//
-	
+	m_canvas->refreshForced();
+}
+
+
+
+void Settings::onDisableDepthPrePass()
+{
+	assert( m_canvas != 0 );
+
+	m_canvas->getGLEngine()->setDepthPrePassEnabled( !m_disableDepthPrePass->get_active() );
+
+	dirtyAllNodes( m_canvas );
+
 	m_canvas->refreshForced();
 }
 
@@ -248,10 +278,17 @@ void Settings::onClearGLResources()
 	assert( m_canvas != 0 );
 
 	// @todo
-	/* m_canvas->getGLEngine()->getGLManager().clear();
-	m_canvas->getGLEngine()->getGLSLManager().clear(); */
-
-	m_canvas->refreshForced();
+	if ( m_canvas->startOpenGLContext() )
+	{
+		m_canvas->getGLEngine()->getGLManager().clear();
+		m_canvas->getGLEngine()->getGLSLManager().clear();
+		dirtyAllNodes( m_canvas );
+		m_canvas->refreshForced();
+	}
+	else
+	{
+		vgAssertN( false, "Unable to clear OpenGL resource. Context not current" );
+	}
 }
 
 
