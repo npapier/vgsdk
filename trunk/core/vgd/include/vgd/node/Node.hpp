22173e1586bcfa6452be8ab209bd832fb1922fb7
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-06-28 09:17:29 +0000

    + Adds new vgd::Wkp template class (similar to vgd::WeakPtr).

--- trunk/core/vgd/include/vgd/node/Node.hpp
+++ trunk/core/vgd/include/vgd/node/Node.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004-2006, 2009, 2010, Nicolas Papier.
+// VGSDK - Copyright (C) 2004-2006, 2009, 2010, 2011, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -31,6 +31,7 @@
 namespace vgd
 {
 	template<class T> struct Shp;
+	template<class T> struct Wkp;
 
 	namespace basic
 	{
@@ -465,6 +466,7 @@ private:
 
 
 typedef vgd::Shp< vgd::node::Node > NodeShp; ///< type definition for shared pointer on a Node
+typedef vgd::Wkp< vgd::node::Node > NodeWkp; ///< type definition for weak pointer on a Node
 
 
 
