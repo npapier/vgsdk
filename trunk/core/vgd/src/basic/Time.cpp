51698de564ee74232569f350090bc45b457a766b
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-07-04 07:38:45 +0000

    ~ Typos.

--- trunk/core/vgd/src/basic/Time.cpp
+++ trunk/core/vgd/src/basic/Time.cpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2009, 2010, Nicolas Papier.
+// VGSDK - Copyright (C) 2009, 2010, 2011, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
