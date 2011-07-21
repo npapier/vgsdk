c97c727e60a4f4bf9abe1eb426f32a3099e4e577
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-07-21 09:15:32 +0000

    ~ Adds the control of orientation (CW, CCW) for vertex and texture coordinates in Grid node.

--- trunk/core/vgd/include/vgd/node/Grid.hpp
+++ trunk/core/vgd/include/vgd/node/Grid.hpp
@@ -53,10 +53,11 @@ struct VGD_API Grid : public vgd::node::VertexShape
 	 * @param height		height of the grid
 	 * @param widthSlices	the grid is subdivided along the width into a number of slices given by this parameter
 	 * @param heightSlices	the grid is subdivided along the height into a number of slices given by this parameter
+	 * @param ccw			specifies the orientation of each quad of the grid
 	 * 
 	 * @remarks Called automatically during node creation.
 	 */
-	void initializeGeometry( const float width = 1.f, const float height = 1.f, const int widthSlices = 1, const int heightSlices = 1 );
+	void initializeGeometry( const float width = 1.f, const float height = 1.f, const int widthSlices = 1, const int heightSlices = 1, const bool ccw = true );
 
 	/**
 	 * @brief Initialize texture coordinates and bindings for all specified textures units.
@@ -64,10 +65,11 @@ struct VGD_API Grid : public vgd::node::VertexShape
 	 * @param numTexUnits	number of desired texture coordinates and bindings
 	 * @param widthSlices	the grid is subdivided along the width into a number of slices given by this parameter
 	 * @param heightSlices	the grid is subdivided along the height into a number of slices given by this parameter
+	 * @param ccw			specifies the texture coordinates orientation of each quad of the grid
 	 *
 	 * @remarks This method could only be called one time.
 	 */
-	void initializeTexUnits( const int32 numTexUnits = 1, const int widthSlices = 1, const int heightSlices = 1 );
+	void initializeTexUnits( const int32 numTexUnits = 1, const int widthSlices = 1, const int heightSlices = 1, const bool ccw = true );
 
 	/**
 	 * @brief Reset the texture coordinates and bindings for all specified textures units.
@@ -75,10 +77,11 @@ struct VGD_API Grid : public vgd::node::VertexShape
 	 * @param numTexUnits	number of desired texture coordinates and bindings
 	 * @param widthSlices	the grid is subdivided along the width into a number of slices given by this parameter
 	 * @param heightSlices	the grid is subdivided along the height into a number of slices given by this parameter
+	 * @param ccw			specifies the texture coordinates orientation of each quad of the grid
 	 *
 	 * @pre initializeTexUnits() must have been called before.
 	 */
-	void resetTextureCoordinates( const int32 numTexUnits = 1, const int widthSlices = 1, const int heightSlices = 1 );
+	void resetTextureCoordinates( const int32 numTexUnits = 1, const int widthSlices = 1, const int heightSlices = 1, const bool ccw = true );
 	//@}
 
 
