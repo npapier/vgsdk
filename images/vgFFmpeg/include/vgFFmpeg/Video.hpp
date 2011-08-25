e54de28312d4b36919a4d7e7bfd6f9060f98fc49
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-08-25 08:03:28 +0000

    ~ Adds getFrameRate() in Video class. ~ VideoCallback class takes care of frame rate of video to play.

--- images/vgFFmpeg/include/vgFFmpeg/Video.hpp
+++ images/vgFFmpeg/include/vgFFmpeg/Video.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2009, 2010, Nicolas Papier.
+// VGSDK - Copyright (C) 2009, 2010, 2011, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -64,6 +64,13 @@ struct VGFFMPEG_API Video
 
 
 	/**
+	 * @brief	Retrieves the fps.
+	 */
+	const float getFrameRate() const;
+
+
+
+	/**
 	 * @brief	Retrieves video duration (in seconds).
 	 */
 	const float getDuration() const;
@@ -105,6 +112,7 @@ protected:
 	AVFormatContext *	pFormatCtx;
 	int					videoStream;
 
+	// video
 	AVCodecContext	*pCodecCtx;
 	AVCodec			*pCodec;
 
