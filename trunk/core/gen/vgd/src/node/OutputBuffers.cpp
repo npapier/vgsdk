04e9a2691703788069fd5ce7b7f285ac5d0e17a5
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-10-24 13:31:28 +0000

    ~ Adds setCurrent(const int, const int) method in OutputBuffer node.

--- trunk/core/gen/vgd/src/node/OutputBuffers.cpp
+++ trunk/core/gen/vgd/src/node/OutputBuffers.cpp
@@ -10,3 +10,23 @@ void OutputBuffers::setCurrent( const int which )
 	}
 	// else nothing to do
 }
+
+
+void OutputBuffers::setCurrent( const int which0, const int which1 )
+{
+	using vgd::field::EditorRW;
+
+	EditorRW< FCurrentType > current = getCurrentRW();
+	current->clear();
+	if ( which0 >= 0 )
+	{
+		current->push_back( which0 );
+	}
+	// else nothing to do
+
+	if ( which1 >= 0 )
+	{
+		current->push_back( which1 );
+	}
+	// else nothing to do
+}
