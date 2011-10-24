a31d9ff2cc589856e8ec0608543dcac2e647aadb
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2011-10-24 13:31:28 +0000

    ~ Adds setCurrent(const int, const int) method in OutputBuffer node.

--- core/gen/vgd/include/vgd/node/OutputBuffers.hpp
+++ core/gen/vgd/include/vgd/node/OutputBuffers.hpp
@@ -9,4 +9,13 @@
 	 * @param which		index of output buffer. Default value -1 means no buffer.
 	 */
 	void setCurrent( const int which = -1 );
+
+
+	/**
+	 * @brief Specifies which buffers must be used for rendering.
+	 *
+	 * @param which0		index of first output buffer.
+	 * @param which1		index of second output buffer.
+	 */
+	void setCurrent( const int which0, const int which1 );
 	//@}
