a6dd3e752b74861c7387d2514eb7dc4f1b57dc0f
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2012-11-20 15:00:13 +0000

    ~ Changes the default zoom in shaders editor. ~ Improves behavior of 'Go To...' line by allowing to close the window with the escape key and by cleaning the line number when the window is hide/reopened (in TextEditorMainWindow class).

--- trunk/ui/vgQt/include/vgQt/Theme.hpp
+++ trunk/ui/vgQt/include/vgQt/Theme.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2012 Alexandre Di Pino.
+// VGSDK - Copyright (C) 2012, Alexandre Di Pino.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Alexandre Di Pino
@@ -7,7 +7,7 @@
 #include "vgQt/vgQt.hpp"
 
 #ifndef _VGQT_THEME_HPP_
-#define  _VGQT_THEME_HPP_
+#define _VGQT_THEME_HPP_
 
 #include <map>
 #include <string>
@@ -16,7 +16,7 @@ namespace vgQt
 {
 
 const std::map<int, int>&	getThemeForeground(const std::string& name);
-	
+
 const std::map<int, int>&	getThemeBackground(const std::string& name);
 
 }
