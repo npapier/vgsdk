576ab35fe6b1bd41d7da52849bbfb88f43d2555e
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2013-07-05 09:06:26 +0000

    ~ Fixes vgd::Shp to support new boost 1.54.0

--- core/vgd/include/vgd/Shp.hpp
+++ core/vgd/include/vgd/Shp.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2004-2006, Nicolas Papier.
+// VGSDK - Copyright (C) 2004-2006, 2013, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -22,6 +22,8 @@ namespace vgd
  * @remarks Written to work with boost version 1.30.2, 1.31.0, 1.32.0 and 1.33.1
  * @sa http://www.boost.org/libs/smart_ptr/shared_ptr.htm
  * 
+ * @todo CXX11
+ *
  * @ingroup g_shp
  */
 template<class T>
@@ -39,7 +41,6 @@ struct Shp : public boost::shared_ptr<T>
 	{}
 
 	// Generated copy constructor, assignment, destructor are fine...
-
 	template<class Y>
 	explicit Shp(boost::weak_ptr<Y> const & r): boost::shared_ptr<T>(r)
 	{}
@@ -48,6 +49,8 @@ struct Shp : public boost::shared_ptr<T>
 	Shp(boost::shared_ptr<Y> const & r): boost::shared_ptr<T>(r)
 	{}
 
+// 1.54.0
+#if SBF_BOOST_VERSION < 1054000
 	template<class Y>
 	Shp(boost::shared_ptr<Y> const & r, boost::detail::static_cast_tag): boost::shared_ptr<T>(r, boost::detail::static_cast_tag())
 	{}
@@ -63,6 +66,7 @@ struct Shp : public boost::shared_ptr<T>
 	template<class Y>
 	Shp(boost::shared_ptr<Y> const & r, boost::detail::polymorphic_cast_tag): boost::shared_ptr<T>(r, boost::detail::polymorphic_cast_tag())
 	{}
+#endif
 
 #ifndef BOOST_NO_AUTO_PTR
 	template<class Y>
@@ -124,7 +128,11 @@ template<class T> inline void swap(Shp<T> & a, Shp<T> & b)
  */
 template<class T, class U> Shp<T> static_pointer_cast(boost::shared_ptr<U> const & r)
 {
+#if SBF_BOOST_VERSION < 1054000
     return Shp<T>(r, boost::detail::static_cast_tag());
+#else
+	return boost::static_pointer_cast<T>(r);
+#endif
 }
 
 
@@ -133,7 +141,11 @@ template<class T, class U> Shp<T> static_pointer_cast(boost::shared_ptr<U> const
  */
 template<class T, class U> Shp<T> const_pointer_cast(boost::shared_ptr<U> const & r)
 {
-    return Shp<T>(r, boost::detail::const_cast_tag());
+#if SBF_BOOST_VERSION < 1054000
+	return Shp<T>(r, boost::detail::const_cast_tag());
+#else
+	return boost::const_pointer_cast<T>(r);
+#endif
 }
 
 
@@ -142,8 +154,24 @@ template<class T, class U> Shp<T> const_pointer_cast(boost::shared_ptr<U> const 
  */
 template<class T, class U> Shp<T> dynamic_pointer_cast(boost::shared_ptr<U> const & r)
 {
-    return Shp<T>(r, boost::detail::dynamic_cast_tag());
+#if SBF_BOOST_VERSION < 1054000
+	return Shp<T>(r, boost::detail::dynamic_cast_tag());
+#else
+	return boost::dynamic_pointer_cast<T>(r);
+#endif
 }
+
+
+#if SBF_BOOST_VERSION >= 1054000
+/**
+ * @relates Shp
+ */
+template<class T, class U> Shp<T> reinterpret_pointer_cast(boost::shared_ptr<U> const & r)
+{
+	return boost::reinterpret_pointer_cast<T>(r);
+}
+#endif
+
 
 
 /**
