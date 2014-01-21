5681eef5017a43b7356794d3df0dfb83371f00f2
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2014-01-21 08:33:14 +0000

    + Adds a new test for LayerPlan. - Removes dependency to vgOpenCollada.

--- test/vgsdkTestGtk/include/helpers.hpp
+++ test/vgsdkTestGtk/include/helpers.hpp
@@ -1,4 +1,4 @@
-// VGSDK - Copyright (C) 2012, Nicolas Papier.
+// VGSDK - Copyright (C) 2012, 2014, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -9,9 +9,17 @@
 #include "vgsdkTestGtk/vgTest/myBase.hpp"
 #include "vgsdkTestGtk/vgTest/myCanvas.hpp"
 
+namespace vgd { namespace node { struct LayerPlan; } }
+
 /**
  * @brief Creates a scene composed of a quad and a matrix of spheres.
  */
 void createQuadAndSpheresMatrix(vgd::Shp< vgsdkTestGtk::vgTest::myBase > base );
+
+
+/**
+ * @brief Creates a LayerPlan node containing an image drawn with cairo (draw a blue disk).
+ */
+vgd::Shp< vgd::node::LayerPlan > createLayerPlan();
 
 #endif // #ifndef _VGSDKTESTGTK_HELPERS_HPP
