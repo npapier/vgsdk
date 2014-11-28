0708a77a056deb58857d8d6c58f5ca4a6af0894f
npapier <npapier@70756234-2556-0410-8cce-5df1b8bfd00f>
2014-11-28 08:35:06 +0000

    - Removes code.

--- trunk/core/vgd/test/src/main.cpp
+++ trunk/core/vgd/test/src/main.cpp
@@ -1,5 +1,3 @@
-
-
 #include "vgdTest/ImageTest.hpp"
 #include <gtest/gtest.h>
 #include <gtest/gtest-spi.h>
@@ -8,8 +6,6 @@
 
 int main( int argc, char ** argv )
 {
-	//char *option[] = { "test.exe", "--gtest_output=xml:report_TEST.xml" };
-	//vgSofa::setDtSofaModelsAsDefaultModels();
 	::testing::InitGoogleTest( &argc, argv );
 	RUN_ALL_TESTS();
 	return 0;
